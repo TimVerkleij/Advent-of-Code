5eec3b8e341b85189faa45643c3d17134f8e8d99
Tim Verkleij <tim.verkleij02@gmail.com>
2023-12-07 15:05:49 +0100

    Add day 7 part 2

---
+++ 2023/day7/part2.js
@@ -0,0 +1,184 @@
+// import text file
+const fs = require('fs')
+let text = fs.readFileSync('input.txt', 'utf-8')
+
+const hands = text.split('\n').map(hand => {
+    let [cards, bid] = hand.split(' ')
+    return {
+        cards,
+        bid
+    }
+})
+
+const cardValues = {
+    'A': '14',
+    'K': '13',
+    'Q': '12',
+    'J': '1',
+    'T': '10',
+}
+
+let types = {
+    'fiveOfaKind': {
+        match: /(\w)\1{4}/,
+        hands: []
+    },
+    'fourOfaKind': {
+        match: /(\w)\1{3}/,
+        hands: []
+    },
+    'fullHouse': {
+        hands: []
+    },
+    'threeOfaKind': {
+        match: /(\w)\1{2}/,
+        hands: []
+    },
+    'twoPair': {
+        hands: []
+
+    },
+    'onePair': {
+        hands: []
+
+    },
+    'highCard': {
+        hands: []
+
+    }
+}
+
+const sortString = (string) => string.split('').sort().join('')
+
+hands.forEach(hand => {
+
+    let cardType = ''
+    if (types.fiveOfaKind.match.test(hand.cards)) {
+
+        cardType = 'fiveOfaKind'
+
+    } else if (types.fourOfaKind.match.test(sortString(hand.cards))) {
+
+        if (hand.cards.includes('J')) {
+            cardType = 'fiveOfaKind'
+        } else {
+            cardType = 'fourOfaKind'
+        }
+        
+    } else if (matchFullHouse(hand.cards)) {
+
+        if (hand.cards.includes('J')) {
+            cardType = 'fiveOfaKind'
+        } else {
+            cardType = 'fullHouse'
+        }
+
+    } else if (types.threeOfaKind.match.test(sortString(hand.cards))) {
+
+        if (hand.cards.includes('J')) {
+            cardType = 'fourOfaKind'
+        } else {
+            cardType = 'threeOfaKind'
+        }
+
+    } else if (matchTwoPair(hand.cards)) {
+
+        if (countJokers(hand.cards) === 1) {
+            cardType = 'fullHouse'
+        } else if (countJokers(hand.cards) === 2) {
+            cardType = 'fourOfaKind'
+        } else {
+            cardType = 'twoPair'
+        }
+
+    } else if (matchOnePair(hand.cards)) {
+
+        if (countJokers(hand.cards) >= 1) {
+            cardType = 'threeOfaKind'
+        } else {
+            cardType = 'onePair'
+        }
+
+    } else if (new Set(hand.cards.split('')).size === 5) {
+
+        if (hand.cards.includes('J')) {
+            cardType = 'onePair'
+        } else {
+            cardType = 'highCard'
+        }
+
+    }
+
+    types[cardType].hands.push({
+        cards: hand.cards,
+        bid: hand.bid
+    })
+
+})
+
+
+function matchFullHouse(string) {
+    const match = sortString(string).match(/(\w)\1{2}/)
+    const differentCharacters = new Set(string.split('')).size
+    if (match && differentCharacters === 2) {
+        return match
+    } else {
+        return false
+    }
+}
+
+function matchTwoPair(string) {
+    const match = sortString(string).match(/(\w)\1{1}/)
+    const differentCharacters = new Set(string.split('')).size
+    if (match && differentCharacters === 3) {
+        return match
+    } else {
+        return false
+    }
+}
+
+function matchOnePair(string) {
+    const match = sortString(string).match(/(\w)\1{1}/)
+    const differentCharacters = new Set(string.split('')).size
+    if (match && differentCharacters === 4) {
+        return match
+    } else {
+        return false
+    }
+}
+
+function countJokers(string) {
+    return string.split('J').length - 1
+}
+
+let sum = 0
+let i = 1
+
+Object.keys(types).reverse().forEach(type => {
+    types[type].hands.sort((a, b) => {
+        let aCards = replaceCardsWithValues(a.cards)
+        let bCards = replaceCardsWithValues(b.cards)
+        for (let [index] of aCards.entries()) {
+            if (aCards[index] === bCards[index]) {
+                continue
+            }
+            return aCards[index] - bCards[index]
+        }
+    })
+
+    types[type].hands.forEach(hand => {
+        sum += parseInt(hand.bid * i)
+        i++
+    })
+
+})
+
+console.log(sum)
+
+function replaceCardsWithValues(string) {
+    const array = string.split('').map(card => {
+        return parseInt(cardValues[card] || card)
+    })
+
+    return array
+}
