1a5fe9e4af66e7ea68757ad56bc276a7dfb7706a
Tim Verkleij <tim.verkleij@vintus.nl>
2024-12-02 09:15:03 +0100

    Add day 2

--- 2024/day2.ts
+++ 2024/day2.ts
@@ -2,20 +2,20 @@ const input = await Bun.file("./input.txt").text();
 
 const lines = input.split("\n");
 const leftList: number[] = [];
-const rightList: number[] = [];
+const rightList: Record<number, number> = [];
 lines.map((x) => {
 	const [left, right] = x.split(/\s+/);
 	leftList.push(+left);
-	rightList.push(+right);
+	// biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
+	rightList[+right] ? rightList[+right]++ : (rightList[+right] = 1);
 });
 
 leftList.sort((a, b) => a - b);
-rightList.sort((a, b) => a - b);
 
 let total = 0;
 
 for (let i = 0; i < leftList.length; i++) {
-	total += Math.abs(leftList[i] - rightList[i]);
+	total += leftList[i] * (rightList[leftList[i]] ?? 0);
 }
 
 console.log(total);
