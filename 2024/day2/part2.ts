d57b2f58c7bd97013c396396ebeb2d1f6655111d
Tim Verkleij <tim.verkleij@vintus.nl>
2024-12-03 10:04:34 +0100

    Add day 2 part 2

---
+++ 2024/day2/part2.ts
@@ -0,0 +1,63 @@
+const input = await Bun.file("./input.txt").text();
+
+// input = `7 6 4 2 1
+// 1 2 7 8 9
+// 9 7 6 2 1
+// 1 3 2 4 5
+// 8 6 4 4 1
+// 1 3 6 7 9`;
+
+const reports = input.split("\n").map((x) => x.split(" ").map((v) => +v));
+
+let count = 0;
+
+for (const report of reports) {
+	const safe = isSafe(report);
+
+	if (safe) {
+		count++;
+		continue;
+	}
+
+	for (let i = 0; i < report.length; i++) {
+		const copy = [...report];
+		copy.splice(i, 1);
+		if (isSafe(copy)) {
+			count++;
+			break;
+		}
+	}
+}
+
+function isSafe(report: number[]) {
+	let isIncreasing: boolean | undefined;
+	let safe = true;
+	for (let i = 1; i < report.length; i++) {
+		const previousLevel = report[i - 1];
+		const currentLevel = report[i];
+
+		if (previousLevel - currentLevel === 0) {
+			safe = false;
+			break;
+		}
+
+		// console.log("isIncreasing: ", isIncreasing);
+		if (isIncreasing === undefined) {
+			isIncreasing = previousLevel - currentLevel < 0;
+		}
+
+		// console.log(previousLevel - currentLevel);
+		if (isIncreasing !== previousLevel - currentLevel < 0) {
+			safe = false;
+			break;
+		}
+
+		if (Math.abs(previousLevel - currentLevel) > 3) {
+			safe = false;
+			break;
+		}
+	}
+	return safe;
+}
+
+console.log(count);
