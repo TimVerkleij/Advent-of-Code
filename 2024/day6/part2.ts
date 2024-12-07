049bd64fb1033b4b958387f261c91500cdae82dc
Tim Verkleij <tim.verkleij@vintus.nl>
2024-12-07 15:48:17 +0100

    Print path with directions

---
+++ 2024/day6/part2.ts
@@ -0,0 +1,74 @@
+const input = await Bun.file("./input.txt").text();
+
+// input = `....#.....
+// .........#
+// ..........
+// ..#.......
+// .......#..
+// ..........
+// .#..^.....
+// ........#.
+// #.........
+// ......#...`;
+
+const pos = {
+	x: 0,
+	y: 0,
+};
+
+const map = input.split("\n").map((x, i) => {
+	let index: number;
+	// biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
+	if ((index = x.indexOf("^")) !== -1) {
+		pos.x = index;
+		pos.y = i;
+	}
+	return x.split("");
+});
+
+const transmutations: Record<direction, { x: number; y: number }> = {
+	N: { x: 0, y: -1 },
+	S: { x: 0, y: 1 },
+	W: { x: -1, y: 0 },
+	E: { x: 1, y: 0 },
+};
+
+const total = 0;
+move(pos.x, pos.y, "N");
+console.log(total);
+
+map.map((x) => {
+	console.log(x.join(""));
+});
+
+function move(x: number, y: number, direction: direction) {
+	const currentTile = map[y][x];
+	if (currentTile === ".") {
+		map[y][x] = direction;
+	}
+	const nextTile =
+		map[y + transmutations[direction].y]?.[x + transmutations[direction].x];
+	if (nextTile === undefined) {
+		return;
+	}
+	if (nextTile === "#") {
+		const newDirection = turn90Degrees(direction);
+		map[y][x] = newDirection;
+		return move(x, y, newDirection);
+	}
+
+	return move(
+		x + transmutations[direction].x,
+		y + transmutations[direction].y,
+		direction,
+	);
+}
+
+function turn90Degrees(direction: direction): direction {
+	if (direction === "N") return "E";
+	if (direction === "E") return "S";
+	if (direction === "S") return "W";
+	return "N";
+}
+
+type direction = "N" | "S" | "W" | "E";
