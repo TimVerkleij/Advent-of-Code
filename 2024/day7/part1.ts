62edd17987428da186fd530f114a507c1d11e648
Tim Verkleij <tim.verkleij@vintus.nl>
2024-12-10 10:47:47 +0100

    Add day 7

--- 2024/day7/part1.ts
+++ 2024/day7/part1.ts
@@ -1,13 +1,49 @@
 const input = await Bun.file("./input.txt").text();
 
-const calibrations = input.split("\n").map((x) => {
+const calibrations: [number, number[]][] = input.split("\n").map((x) => {
 	const [testValue, numbers] = x.split(": ");
-	const parsedNumbers = numbers.split(" ").map((x) => +x);
+	const parsedNumbers: number[] = numbers.split(" ").map((x) => +x);
 	return [+testValue, parsedNumbers];
 });
 
-const operators = ["*", "+"];
-
+const operators = ["+", "*"];
+let total = 0;
 for (const [testValue, numbers] of calibrations) {
-	console.log(testValue, numbers);
+	// const operatorOrder = new Array(numbers.length - 1).fill(operators[0]);
+	const max = Number.parseInt("1".repeat(numbers.length - 1), 2);
+	// console.log(max);
+	// console.log(operatorOrder);
+	let isValid = false;
+	for (let i = 0b0; i <= max; i++) {
+		const operatorOrder = i
+			.toString(2)
+			.padStart(numbers.length - 1)
+			.split("")
+			.map((x) => operators[+x]);
+		// console.log(operatorOrder);
+
+		let result = numbers[0];
+		for (let j = 0; j < operatorOrder.length; j++) {
+			const op = operatorOrder[j];
+			const nextNumber = numbers[j + 1];
+			if (op === "+") {
+				result += nextNumber;
+			} else if (op === "*") {
+				result *= nextNumber;
+			}
+
+			if (result > testValue) {
+				break;
+			}
+		}
+		if (result === testValue) {
+			isValid = true;
+			break;
+		}
+	}
+	if (isValid) {
+		total += testValue;
+	}
+	// console.log(testValue, numbers);
 }
+console.log(total);
