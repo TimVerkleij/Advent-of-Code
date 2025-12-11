9b714c94d6e8ec2b47329f413f2b7869f9e68503
Tim Verkleij <tim.verkleij02@gmail.com>
2025-12-11 21:26:20 +0100

    Finish day 6 part 2

--- 2025/day6/part2.ts
+++ 2025/day6/part2.ts
@@ -1,46 +1,33 @@
 let input = await Bun.file('input.txt').text()
 
-input = `123 328  51 64 
- 45 64  387 23 
-  6 98  215 314
-*   +   *   +  `
+// input = `123 328  51 64 
+//  45 64  387 23 
+//   6 98  215 314
+// *   +   *   +  `
 
-const problems: string[][] = []
-const lines = input.split('\n')
-const numbers = lines.map((x, i) => {
-    if (i == lines.length - 1)
-        return x.trim().split(/\s+/g)
+const lines = input.split('\n').map(x => x + ' ')
+const operators = lines.pop()!
 
-    return x.match(/\d+\s*/g)
-})
+let sum = 0
 
-// console.log(numbers)
-for (const list of numbers) {
-    // console.log(list)
-    list?.map((x, i) => problems[i] ? problems[i].push(x) : problems[i] = [x])
-}
-
-
-for (const problem of problems) {
-    const operator = problem.pop()
-    const numbers: string[] = []
-
-    console.log(problem)
-    let numberCount = Math.max(...problem.map(x => x.length))
-    for (let i = 0; i < numberCount; i++) {
-        for (const number of problem) {
-            const endIndex = -(i + 1) + 1 == 0 ? undefined : -(i + 1) + 1
-            // console.log(-(i + 1), endIndex)
-            const addedNumber = number.slice(-(i + 1), endIndex)
-            if (numbers[i]) {
-                numbers[i] += addedNumber
-            } else {
-                numbers[i] = addedNumber
-            }
-        }
+let currentOperator
+let currentProblem: string[] = []
+for (let i = 0; i < lines[0].length; i++) {
+    if (operators[i] != ' ') {
+        currentOperator = operators[i]
     }
-    // console.log(numbers)
+    let number = lines.map(x => x[i]).join('').trim()
 
+    if (!number) {
+        const problem = currentProblem.join(currentOperator)
+        sum += eval(problem)
+        // console.log(problem, '=', eval(problem))
+        currentProblem = []
+        continue
+    }
 
-    // console.log(problem.map(x => x.length))
+    currentProblem.push(number)
+
 }
+
+console.log(sum)
