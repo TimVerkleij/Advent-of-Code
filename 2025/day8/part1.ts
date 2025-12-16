225f99c318e8999151aafe54b57ea26669872a00
TimVerkleij <tim.verkleij@vintus.nl>
2025-12-16 10:18:58 +0100

    WIP: day 8 create groups

--- 2025/day8/part1.ts
+++ 2025/day8/part1.ts
@@ -1,5 +1,26 @@
 let input = await Bun.file('input.txt').text()
 
+input = `162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689`
+
 type Box = {
     x: number,
     y: number,
@@ -59,6 +80,9 @@ for (const [cell, boxes] of grid) {
         for (let j = i + 1; j < compareList.length; j++) {
             const compareBox = compareList[j]
             const distance = calculateDistance(currentBox, compareBox)
+            if (distance === 0) {
+                continue
+            }
             if (distance < closestDistance) {
                 closestBox = compareBox
                 closestDistance = distance
@@ -81,18 +105,62 @@ for (const [cell, boxes] of grid) {
 
 const groups: Set<string>[] = []
 
+const maxConnections = 10
+let connectionCount = 0
+
+console.log(pairs)
+
 for (const [box1, box2] of pairs) {
+    if (connectionCount === maxConnections) {
+        break
+    }
+
     const stringifiedBox1 = `${box1.x},${box1.y},${box1.z}`
     const stringifiedBox2 = `${box2.x},${box2.y},${box2.z}`
 
-    // groupIndexForBox1
+    const groupIndexForBox1 = groups.findIndex(x => x.has(stringifiedBox1))
+    const groupIndexForBox2 = groups.findIndex(x => x.has(stringifiedBox2))
 
-    for (let i = 0; i < groups.length; i++) {
-        const group = groups[i]
-
+    // Neither one is in a group yet
+    if (groupIndexForBox1 + groupIndexForBox2 === -2) {
+        connectionCount++
+        groups.push(new Set([stringifiedBox1, stringifiedBox2]))
+        continue
     }
 
+    // They're in the same group
+    if (groupIndexForBox1 === groupIndexForBox2) {
+        continue
+    }
+
+
+    // only box two is in a group
+    if (groupIndexForBox1 === -1) {
+        connectionCount++
+        groups[groupIndexForBox2].add(stringifiedBox1)
+        continue
+    }
+
+    // only box one is in a group
+    if (groupIndexForBox2 === -1) {
+        connectionCount++
+        groups[groupIndexForBox1].add(stringifiedBox2)
+        continue
+    }
+
+    // They are in different groups (time to merge)
+
+    const group1 = groups[groupIndexForBox1]
+    const group2 = groups[groupIndexForBox2]
+
+    groups.push(group1.union(group2))
+
+    groups.splice(groupIndexForBox1, 1)
+    groups.splice(groupIndexForBox2, 1)
+    connectionCount++
 }
+
+console.log(groups)
 // Check duplicates
 // const list = pairs.map(([first, second]) => {
 //     const { x: x1, y: y1, z: z1 } = first
