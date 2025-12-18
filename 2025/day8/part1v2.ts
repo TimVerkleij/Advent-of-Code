1de4e50af23c733f138285f9883f65290c3b1200
TimVerkleij <tim.verkleij@vintus.nl>
2025-12-18 14:15:55 +0100

    Day 8 part 1

--- 2025/day8/part1v2.ts
+++ 2025/day8/part1v2.ts
@@ -1,25 +1,25 @@
 let input = await Bun.file('input.txt').text()
 
-input = `162,817,812
-57,618,57
-906,360,560
-592,479,940
-352,342,300
-466,668,158
-542,29,236
-431,825,988
-739,650,466
-52,470,668
-216,146,977
-819,987,18
-117,168,530
-805,96,715
-346,949,466
-970,615,88
-941,993,340
-862,61,35
-984,92,344
-425,690,689`
+// input = `162,817,812
+// 57,618,57
+// 906,360,560
+// 592,479,940
+// 352,342,300
+// 466,668,158
+// 542,29,236
+// 431,825,988
+// 739,650,466
+// 52,470,668
+// 216,146,977
+// 819,987,18
+// 117,168,530
+// 805,96,715
+// 346,949,466
+// 970,615,88
+// 941,993,340
+// 862,61,35
+// 984,92,344
+// 425,690,689`
 
 class Box {
     readonly x: number
@@ -97,9 +97,9 @@ let connectionCount = 0
 
 for (const vertex of graph) {
     const createdLinked = tree.union(vertex[0], vertex[1])
+    connectionCount++
     if (createdLinked) {
-        connectionCount++
-        if (connectionCount >= 10) {
+        if (connectionCount >= 1000) {
             break
         }
     }
@@ -109,8 +109,9 @@ for (const vertex of graph) {
 
 const obj: Record<string, number> = {}
 
-for (const value of tree.root.values()) {
-    obj[value] ? obj[value]++ : obj[value] = 1
+for (const key of tree.root.keys()) {
+    const root = tree.findRoot(key)
+    obj[root] = (obj[root] ?? 0) + 1
 }
 
 const groupSizes = Object.values(obj).sort((a, b) => b - a)
